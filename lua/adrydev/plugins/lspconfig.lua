1d1850beeec5b456ca01ac08cd4a918c2f9c159d
= <=>
2024-07-25 19:31:52 -0400

    Added vue lsp

--- lua/adrydev/plugins/lspconfig.lua
+++ lua/adrydev/plugins/lspconfig.lua
@@ -1,67 +1,86 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    { "folke/neodev.nvim", opts = {} },
-  },
-  config = function()
-    local nvim_lsp = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	config = function()
+		local nvim_lsp = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local protocol = require("vim.lsp.protocol")
+		local protocol = require("vim.lsp.protocol")
 
-    local on_attach = function(client, bufnr)
-      -- format on save
-      if client.server_capabilities.documentFormattingProvider then 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("Format", { clear = true }),
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format()
-          end,
-        })
-      end
-    end
+		local on_attach = function(client, bufnr)
+			-- format on save
+			if client.server_capabilities.documentFormattingProvider then
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = vim.api.nvim_create_augroup("Format", { clear = true }),
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format()
+					end,
+				})
+			end
+		end
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local mason_registry = require("mason-registry")
+		local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+			.. "/node_modules/@vue/language-server"
 
-    mason_lspconfig.setup_handlers({
-      function(server)
-        nvim_lsp[server].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["tsserver"] = function()
-        nvim_lsp["tsserver"].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end,
-      ["cssls"] = function()
-        nvim_lsp["cssls"].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-      })
-      end,
-      ["html"] = function()
-        nvim_lsp["html"].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end,
-      ["jsonls"] = function()
-        nvim_lsp["jsonls"].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end,
-      ["eslint"] = function()
-        nvim_lsp["eslint"].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end,
-    })
-  end,
+		mason_lspconfig.setup_handlers({
+			function(server)
+				nvim_lsp[server].setup({
+					capabilities = capabilities,
+				})
+			end,
+			["tsserver"] = function()
+				nvim_lsp["tsserver"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+					init_options = {
+						plugins = {
+							{
+								name = "@vue/typescript-plugin",
+								location = vue_language_server_path,
+								languages = { "vue" },
+							},
+						},
+					},
+					filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+				})
+			end,
+			["volar"] = function()
+				nvim_lsp["volar"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+			["cssls"] = function()
+				nvim_lsp["cssls"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+			["html"] = function()
+				nvim_lsp["html"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+			["jsonls"] = function()
+				nvim_lsp["jsonls"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+			["eslint"] = function()
+				nvim_lsp["eslint"].setup({
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end,
+		})
+	end,
 }
