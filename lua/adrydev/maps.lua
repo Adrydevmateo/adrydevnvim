28527cff280999fe2dbaed7d6142ad0f9a84fc96
= <=>
2024-07-24 21:53:20 -0400

    Removed map(Exit insert mode)

--- lua/adrydev/maps.lua
+++ lua/adrydev/maps.lua
@@ -8,9 +8,6 @@ map("n", "<leader>w", "<CMD>update<CR>")
 -- Quit
 map("n", "<leader>q", "<CMD>q<CR>")
 
--- Exit insert mode
-map("i", "jk", "<ESC>")
-
 -- NeoTree
 map("n", "<leader>e", "<CMD>Neotree toggle<CR>")
 map("n", "<leader>r", "<CMD>Neotree focus<CR>")
