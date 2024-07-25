7f17bc62a04f217031d55d17d7b40c6972e3aca7
= <=>
2024-07-24 22:02:02 -0400

    Changed toggleterm mapping

--- lua/adrydev/plugins/toggleterm.lua
+++ lua/adrydev/plugins/toggleterm.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("toggleterm").setup({
 			size = 10,
-			open_mapping = [[<F7>]],
+			open_mapping = [[<C-\>]],
 			shading_factor = 2,
 			direction = "float",
 			float_opts = {
