3fd712bb3c84e0e44a2c092edeaa52e97fe8e651
AdryDev <adrydevmateo@gmail.com>
2024-08-18 21:28:42 -0400

    Changed startup plugin

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -95,11 +95,43 @@ return {
 	},
 	-- Startup
 	{
-		"startup-nvim/startup.nvim",
-		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
 		config = function()
-			require("startup").setup()
+			require("dashboard").setup({
+				-- config
+				theme = "hyper",
+				config = {
+					week_header = {
+						enable = true,
+					},
+					shortcut = {
+						{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+						{
+							icon = " ",
+							icon_hl = "@variable",
+							desc = "Files",
+							group = "Label",
+							action = "Telescope find_files",
+							key = "f",
+						},
+						{
+							desc = " Apps",
+							group = "DiagnosticHint",
+							action = "Telescope app",
+							key = "a",
+						},
+						{
+							desc = " dotfiles",
+							group = "Number",
+							action = "Telescope dotfiles",
+							key = "d",
+						},
+					},
+				},
+			})
 		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
 	},
 	-- Statusline
 	{
